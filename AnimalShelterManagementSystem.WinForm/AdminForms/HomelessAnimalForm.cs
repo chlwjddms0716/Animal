56af1001d76b91f2765e988c407aaa218f26864b
Hyelimione <gmladk402@gmail.com>
2020-07-08 09:47:13 +0900

    커밋 테스트

--- AnimalShelterManagementSystem.WinForm/AdminForms/HomelessAnimalForm.cs
+++ AnimalShelterManagementSystem.WinForm/AdminForms/HomelessAnimalForm.cs
@@ -37,10 +37,14 @@ namespace AnimalShelterManagementSystem.WinForm.Forms
             string checkinput = "";
             if (string.Equals(checkinput, "") == true)
             {
-                _homelessAnimal.HomelessAnimalId = DataRepository.HomelessAnimal.GetMaxId() + 1;
+              _homelessAnimal.HomelessAnimalId = DataRepository.HomelessAnimal.GetMaxId() + 1;
+                _homelessAnimal.Name = txeName.Text;
+                _homelessAnimal.Age = Convert.ToInt32(txeAge.Text);
+                
                 _homelessAnimal.Feature = txeFeature.Text;
                 _homelessAnimal.LatestFindingReport = dteDate.DateTime.Date;
                 _homelessAnimal.PictureLink = txePictureLink.Text;
+
 
                 //MessageBox.Show("저장되었습니다.");
 
@@ -57,7 +61,6 @@ namespace AnimalShelterManagementSystem.WinForm.Forms
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-       
 
             try
             {
@@ -91,17 +94,10 @@ namespace AnimalShelterManagementSystem.WinForm.Forms
         {
 
             txeHomelessAnimalId.Text = Convert.ToString(_homelessAnimal.HomelessAnimalId + 1);
-
             txeName.Text = _homelessAnimal.Name;
-
-    
             txeFeature.Text = _homelessAnimal.Feature;
             dteDate.Text = Convert.ToString(_homelessAnimal.LatestFindingReport);
             txePictureLink.Text = _homelessAnimal.PictureLink;
-
-
-
-
 
         }
     }
