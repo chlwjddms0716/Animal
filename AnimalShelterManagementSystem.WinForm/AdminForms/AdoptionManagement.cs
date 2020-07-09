48b634ba50f6f24a15ae381bafa600155fb0167e
성수 장 <tjdtn1007@naver.com>
2020-07-09 11:47:21 +0900

    d

--- AnimalShelterManagementSystem.WinForm/AdminForms/AdoptionManagement.cs
+++ AnimalShelterManagementSystem.WinForm/AdminForms/AdoptionManagement.cs
@@ -1,4 +1,5 @@
 ﻿using AnimalShelterManagementSystem.Data;
+using AnimalShelterManagementSystem.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,7 +14,11 @@ namespace AnimalShelterManagementSystem.WinForm.AdminForms
 {
     public partial class AdoptionManagement : Form
     {
-        private int userId;
+        List<Adoption> adoptionList = new List<Adoption>();
+        List<Adoption> FilteredById;
+        List<Adoption> FilteredByAdoptionStatus;
+        private string Id;
+        private int currentStatus;
         Adoption adoption = new Adoption();
 
         public AdoptionManagement()
@@ -21,35 +26,106 @@ namespace AnimalShelterManagementSystem.WinForm.AdminForms
             InitializeComponent();
         }
 
-        public AdoptionManagement(int UserId) : this()
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            userId = UserId;
+
+        }
+        private void querybyId()
+        {
+            if (String.Equals(txeId.Text, "") == false)
+                FilteredById = adoptionList.Where(x => x.userLoginId.Contains(txeId.Text) == true).ToList();
+            else
+                FilteredById = adoptionList;
+        }
+        private void querybyAdoptionStatus()
+        {
+            querybyId();
+            if (currentStatus != 2)
+                FilteredByAdoptionStatus = FilteredById.Where(x => x.AdoptionStatus == (AdoptionStatusType)currentStatus).ToList();
+            else
+                FilteredByAdoptionStatus = FilteredById;
+            homelessAnimalBindingSource.DataSource = FilteredByAdoptionStatus;
         }
 
-        private void btnAdoptionManagement_Click(object sender, EventArgs e)
+        private void AdoptionManagement_Load(object sender, EventArgs e)
         {
+            adoptionList = DataRepository.Adoption.GetEvery();
+            querybyAdoptionStatus();
+        }
 
-            /*
-            if (String.Equals(txeUserName.Text, "") == true)
-            {
-                MessageBox.Show("유저아이디를 입력해주세요");
-            }
-            */
+        private void tsbAdd_Click(object sender, EventArgs e)
+        {
+            ExcuteInsert();
+        }
 
+        private void tsbDelete_Click(object sender, EventArgs e)
+        {
+            ExecuteDelete();
+        }
+
+        private void tsbEdit_Click(object sender, EventArgs e)
+        {
+            ExecuteEdit();
+        }
+
+        private void ExcuteInsert()
+        {
+            AdoptionForm form = new AdoptionForm(adoption, 1);
+            form.ShowDialog();
+        }
+
+        private void ExecuteDelete()
+        {
             List<HomelessAnimal> animals = DataRepository.Adoption.GetAnimalsByUserName(txeUserName.Text);
 
             homelessAnimalBindingSource.DataSource = animals;
 
+
+            //if (String.Equals(tebUserId.Text, "") == true)
+            //{
+            //    MessageBox.Show("유저아이디를 입력해주세요");
+            //}
+            /*
+            adoption.UserId = userId;
+            DataRepository.Adoption.Insert(adoption);
+            HomelessAnimal homelessAnimal = new HomelessAnimal();
+            homelessAnimal = DataRepository.HomelessAnimal.Get(adoption.HomelessAnimalId);
+            homelessAnimal.IsAdopted = true;
+            DataRepository.HomelessAnimal.Update(homelessAnimal);
+
+            userId = DataRepository.HomelessAnimal.GetMaxId() + 1;
+            userId.Species = Convert.ToInt32(cbbSpecies.SelectedValue);
+            _homelessAnimal.Age = Convert.ToInt32(txeAge.Text);
+            _homelessAnimal.Species = Convert.ToInt32(cbbSpecies.SelectedValue);
+            _homelessAnimal.Feature = txeFeature.Text;
+            _homelessAnimal.Gender = Convert.ToInt32(cbbGender.SelectedValue);
+            _homelessAnimal.PhysicalCondition = Convert.ToInt32(cbbPSC.SelectedValue);
+            _homelessAnimal.LatestFindingReport = dteLatestFindingReport.DateTime.Date;
+            _homelessAnimal.PictureLink = txePictureLink.Text;
+            _homelessAnimal.AnimalShelterId = Convert.ToInt32(cbbAnimalShelter.SelectedValue);
+           
+             */
+
+            AdoptionForm form = new AdoptionForm(adoption, 0);
+            form.ShowDialog();
         }
 
-        private void btnCancel_Click(object sender, EventArgs e)
+        private void tsbRefresh_Click(object sender, EventArgs e)
         {
-            Close();
+            adoptionList = DataRepository.Adoption.GetEvery();
+            querybyAdoptionStatus();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void txeId_TextChanged(object sender, EventArgs e)
         {
+            Id = txeId.Text;
+            querybyAdoptionStatus();
+        }
 
+        private void rdgAdoptionStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentStatus = (int)rdgAdoptionStatus.EditValue;
+            querybyAdoptionStatus();
         }
     }
 }
--- AnimalShelterManagementSystem.WinForm/AdminForms/HomelessAnimalForm.designer.cs
+++ AnimalShelterManagementSystem.WinForm/AdminForms/HomelessAnimalForm.designer.cs
@@ -1,4 +1,4 @@
-﻿namespace AnimalShelterManagementSystem.WinForm.Forms
+namespace AnimalShelterManagementSystem.WinForm.Forms
 {
     partial class HomelessAnimalForm
     {
@@ -203,7 +203,7 @@
             this.btnSave.Size = new System.Drawing.Size(386, 34);
             this.btnSave.StyleController = this.layoutControl1;
             this.btnSave.TabIndex = 14;
-            this.btnSave.Text = "추가";
+            this.btnSave.Text = "저장(&S)";
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnClose
@@ -216,7 +216,7 @@
             this.btnClose.Size = new System.Drawing.Size(386, 34);
             this.btnClose.StyleController = this.layoutControl1;
             this.btnClose.TabIndex = 13;
-            this.btnClose.Text = "취소";
+            this.btnClose.Text = "취소(&C)";
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // txeFeature
@@ -386,7 +386,11 @@
             this.layoutControlItem6.Name = "layoutControlItem6";
             this.layoutControlItem6.Size = new System.Drawing.Size(780, 30);
             this.layoutControlItem6.Text = "보호센터 :";
-            this.layoutControlItem6.TextSize = new System.Drawing.Size(101, 18);
+            this.layoutControlItem6.TextSize = new System.Drawing.Size(107, 18);
+            // 
+            // animalShelterBindingSource
+            // 
+            this.animalShelterBindingSource.DataSource = typeof(AnimalShelterManagementSystem.AnimalShelter);
             // 
             // homelessAnimalBindingSource
             // 
