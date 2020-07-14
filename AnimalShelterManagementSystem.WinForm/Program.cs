e1b6a7ebde5ce068ea745b69e722a53650d85bf9
chlwjddms0716 <63761322+chlwjddms0716@users.noreply.github.com>
2020-07-14 09:32:47 +0900

    마지막 합본

--- AnimalShelterManagementSystem.WinForm/Program.cs
+++ AnimalShelterManagementSystem.WinForm/Program.cs
@@ -29,7 +29,7 @@ namespace AnimalShelterManagementSystem.WinForm
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
            //Application.Run(new AdminMenu());
-            Application.Run(new UserMenu());
+           Application.Run(new UserMenu());
         }
 
 
